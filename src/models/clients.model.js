f7078fa54ae285b6cae8f848b290141de50392dc
Juan M. Garcia <garcia.solutions@gmail.com>
2018-10-02 15:44:29 -0500

    initial commit

---
+++ src/models/clients.model.js
@@ -0,0 +1,15 @@
+// clients-model.js - A mongoose model
+// 
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+  const clients = new Schema({
+    text: { type: String, required: true }
+  }, {
+    timestamps: true
+  });
+
+  return mongooseClient.model('clients', clients);
+};
